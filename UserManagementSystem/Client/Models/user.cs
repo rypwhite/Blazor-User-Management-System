4a5313816d5bcf5fd0bc84c9997d0046f2801281
Ryan White <48073843+rypwhite@users.noreply.github.com>
2023-06-20 18:48:11 +0100

    Class constructor for creating user objects

--- UserManagementSystem/Client/Models/user.cs
+++ UserManagementSystem/Client/Models/user.cs
@@ -7,4 +7,15 @@ public class User
     public string? Address { get; set; }
     public string? EmailAddress { get; set; }
     public string? PhoneNumber { get; set; }
+
+    public User(int uid, string firstName, string lastName, int age, string address, string emailAddress, string phoneNumber)
+    {
+        UID = uid;
+        FirstName = firstName;
+        LastName = lastName;
+        Age = age;
+        Address = address;
+        EmailAddress = emailAddress;
+        PhoneNumber = phoneNumber;
+    }
 }
