676c3317896c772b3a6b134ac2be548ecc53ff87
Ryan White <48073843+rypwhite@users.noreply.github.com>
2023-06-20 18:46:38 +0100

    Implementing non-null value for our User class

--- UserManagementSystem/Client/Models/user.cs
+++ UserManagementSystem/Client/Models/user.cs
@@ -1,10 +1,10 @@
 public class User
 {
     public int UID { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
     public int Age { get; set; }
-    public string Address { get; set; }
-    public string EmailAddress { get; set; }
-    public string PhoneNumber { get; set; }
+    public string? Address { get; set; }
+    public string? EmailAddress { get; set; }
+    public string? PhoneNumber { get; set; }
 }
