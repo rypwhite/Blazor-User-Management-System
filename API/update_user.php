1d58bebf4bbb54ccd5247c3eefd3d33a275e756e
Ryan White <48073843+rypwhite@users.noreply.github.com>
2023-06-23 16:11:55 +0100

    Updating users using set variables and assigning set variables on input change

--- API/update_user.php
+++ API/update_user.php
@@ -6,14 +6,14 @@ header("Access-Control-Allow-Headers: Content-Type");
 require("sql_wrapper.php");
 
 
-if (!isset($_GET["UID"]) || !isset($_GET["FirstName"]) || isset($_GET["LastName"]) || isset($_GET["Age"]) ||isset($_GET["Address"]) ||isset($_GET["Email"]) ||isset($_GET["Phone"])) {
+if (!isset($_GET["UID"]) || !isset($_GET["FirstName"]) || !isset($_GET["LastName"]) || !isset($_GET["Age"]) || !isset($_GET["Address"]) || !isset($_GET["Email"]) || !isset($_GET["Phone"])) {
     http_response_code(400);
     $response = array("status" => "error", "message" => "missing information from request");
     die(json_encode($response));
 }
 
 $UID = $_GET["UID"];
-$firstName = $_GET["firstName"];
+$FirstName = $_GET["FirstName"];
 $LastName = $_GET["LastName"];
 $Age = $_GET["Age"];
 $Address = $_GET["Address"];
@@ -22,7 +22,7 @@ $Phone = $_GET["Phone"];
 
 try {
     $sql = new sql_wrapper("plesk.remote.ac", "WS301022_x_api_1nf89n913f", "cB?w66z79", "WS301022_x_api");
-    $sql->query("UPDATE `users` SET `FirstName` = ?,`LastName` = ?,`Age` = ?,`Address` = ?,`Email` = ?,`Phone` = ? WHERE `UID` = ?", [$firstName, $lastName, $Age, $Address, $Email, $Phone, $UID]);
+    $sql->query("UPDATE `users` SET `FirstName` = ?,`LastName` = ?,`Age` = ?,`Address` = ?,`Email` = ?,`Phone` = ? WHERE `UID` = ?", [$FirstName, $LastName, $Age, $Address, $Email, $Phone, $UID]);
 } catch (Exception $e) {
     http_response_code(500);
     $response = array("status" => "error", "message" => $e->getMessage());
