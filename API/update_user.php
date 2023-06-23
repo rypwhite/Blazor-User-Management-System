6c3716d9633a75f3589413fe95f92e1e1526e6f5
Ryan White <48073843+rypwhite@users.noreply.github.com>
2023-06-23 15:19:58 +0100

    Changing update endpoint to accept url parameters also

--- API/update_user.php
+++ API/update_user.php
@@ -5,21 +5,20 @@ header("Access-Control-Allow-Headers: Content-Type");
 
 require("sql_wrapper.php");
 
-$data = json_decode(file_get_contents('php://input'), true);
 
-if (!isset($data["UID"]) || !isset($data["FirstName"]) || isset($data["LastName"]) || isset($data["Age"]) ||isset($data["Address"]) ||isset($data["Email"]) ||isset($data["Phone"])) {
+if (!isset($_GET["UID"]) || !isset($_GET["FirstName"]) || isset($_GET["LastName"]) || isset($_GET["Age"]) ||isset($_GET["Address"]) ||isset($_GET["Email"]) ||isset($_GET["Phone"])) {
     http_response_code(400);
     $response = array("status" => "error", "message" => "missing information from request");
     die(json_encode($response));
 }
 
-$UID = $data["UID"];
-$firstName = $data["firstName"];
-$LastName = $data["LastName"];
-$Age = $data["Age"];
-$Address = $data["Address"];
-$Email = $data["Email"];
-$Phone = $data["Phone"];
+$UID = $_GET["UID"];
+$firstName = $_GET["firstName"];
+$LastName = $_GET["LastName"];
+$Age = $_GET["Age"];
+$Address = $_GET["Address"];
+$Email = $_GET["Email"];
+$Phone = $_GET["Phone"];
 
 try {
     $sql = new sql_wrapper("plesk.remote.ac", "WS301022_x_api_1nf89n913f", "cB?w66z79", "WS301022_x_api");
